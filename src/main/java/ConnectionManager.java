014af9638839559329b1f54c501281c4b4160929
alex <sasha.lazoriak@gmail.com>
2020-09-12 14:35:52 +0200

    [add] Added method connectToDB

--- src/main/java/ConnectionManager.java
+++ src/main/java/ConnectionManager.java
@@ -1,8 +1,33 @@
 import org.apache.log4j.*;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 
 public class ConnectionManager {
     final static Logger logger = Logger.getLogger(ConnectionManager.class);
+
+    public String author ="Oleksandr";
+    public String query = "SELECT VERSION()";
+
+
+
+    public void connectToDB(String url, String user, String password){
+
+        try {
+            Connection connection = DriverManager.getConnection(url,user,password);// створення підключення до перщої бази даних
+            logger.debug("Підключення до бази даних");
+            logger.debug("Відбулося підключення до бази даних: ");
+            connection.isValid(25);// записати в дата сторадж
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            logger.error("SQL Error", throwables);//.fillInStackTrace()
+        }
+
+    }
+
 
     /**
      * Тут потрібно написати підключення до перщої бази
