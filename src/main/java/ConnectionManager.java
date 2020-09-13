d829efcea4d1a05ef30618a1395e12a00b222bca
alex <sasha.lazoriak@gmail.com>
2020-09-13 19:15:21 +0200

    [add] Added log

--- src/main/java/ConnectionManager.java
+++ src/main/java/ConnectionManager.java
@@ -1,4 +1,5 @@
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -6,32 +7,35 @@ import java.sql.SQLException;
 
 
 public class ConnectionManager {
-    final static Logger logger = Logger.getLogger(ConnectionManager.class);
+    private static final Logger getLog = LogManager.getLogger(ConnectionManager.class);
 
     Connection connection;
     ConnectionPool connectionPool = new ConnectionPool();
 
 
-
     public Connection connectToDB() throws SQLException {
         Connection activeConnection = null;// global variable to return active connect to DB
         try{
+            getLog.debug("Connecting to master DB...");
             activeConnection = connection = DriverManager.getConnection(
                     connectionPool.getMasterUrl(),
                     connectionPool.getUser(),
                     connectionPool.getPassword()
             );
-
+            getLog.debug("Connected to master DB");
             connection.setAutoCommit(false);
         } catch (SQLException e) {
             try {
+                getLog.trace("Can't connect to master DB, connecting to reserve DB...", e);
                  activeConnection = connection = DriverManager.getConnection(
                         connectionPool.getSecondUrl(),
                         connectionPool.getUser(),
                         connectionPool.getPassword()
                 );
+                getLog.trace("Connected to reserve DB");
             } catch (SQLException throwables) {
                 throwables.printStackTrace();
+                getLog.error("Can't connect to reserve DB", throwables);
             }
             connection.setAutoCommit(false);
         }
