837c6299bdb3862db63dfa8d909177ff45037e68
alex <sasha.lazoriak@gmail.com>
2020-09-13 15:53:42 +0200

    [change] Changed print in console

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,26 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello");
+    public static void main(String[] args) throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+        DataFromDB dataFromDB = new DataFromDB();
         ConnectionManager connectionManager = new ConnectionManager();
-        ConnectionPool connectionPool = new ConnectionPool();
-        connectionManager.connectToDB(connectionPool.getUrl(),connectionPool.getUser(),connectionPool.getPassword());
+        boolean loop= true;
+        while (loop){
+            System.out.println("Choose one");
+            System.out.println("1: request to DB(get person name) \n2: exit");
+            int number = scanner.nextInt();  // Read user input
+            switch (number)
+            {
+                case 1 : Connection connection=connectionManager.connectToDB();
+                    dataFromDB.getNameDataFromDB(connection);
+                    break;
+                case 2 : loop = false;
+                    break;
+            }
+        }
+
     }
 }
