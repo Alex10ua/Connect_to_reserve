22215bb22f620c456448597fd4967c325b9f5eb3
alex <sasha.lazoriak@gmail.com>
2020-09-14 01:59:57 +0200

    [add] Added comments

--- src/main/java/app/ConnectionManager.java
+++ src/main/java/app/ConnectionManager.java
@@ -1,11 +1,9 @@
 package app;
 
-
 import org.apache.log4j.Logger;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 
 public class ConnectionManager {
     private static final Logger getLog= Logger.getLogger(ConnectionManager.class);
@@ -14,7 +12,6 @@ public class ConnectionManager {
     private static Connection activeConnection; // global variable to return active connect to DB
     public ConnectionManager() {
     }
-
     public Connection connectToDB() throws SQLException {
         if (activeConnection==null) {
             try {
@@ -23,43 +20,29 @@ public class ConnectionManager {
                         connectionPool.getMasterUrl(),
                         connectionPool.getUser(),
                         connectionPool.getPassword()
-                );
+                );                                          //getting connection to masterDB
                 activeConnection = connection;
                 getLog.info("Connected to master DB");
                 activeConnection.setAutoCommit(false);//disable auto-commit mode
             } catch (SQLException e) {
+                getLog.error("Can't connect to master DB", e);
                 try {
-                    getLog.error("Can't connect to master DB", e);
                     getLog.info("Connecting to reserve DB...");
                     connection = DriverManager.getConnection(
                             connectionPool.getSecondUrl(),
                             connectionPool.getUser(),
                             connectionPool.getPassword()
-                    );
+                    );                                      //getting connection to reserveDB
                     activeConnection = connection;
                     getLog.info("Connected to reserve DB");
-                    //запустити тут потік для перевірки чи доступна 1 база
+                    activeConnection.setAutoCommit(false);//disable auto-commit mode
                 } catch (SQLException throwables) {
-                    throwables.printStackTrace();
+                    activeConnection=null;// if all DB are not available
                     getLog.error("Can't connect to reserve DB", throwables);
                 }
-                activeConnection.setAutoCommit(false);//disable auto-commit mode
+
             }
         }
     return activeConnection;    
-    }
-
-    private void checkMasterDB(){
-        try {
-            Connection testMasterDB = DriverManager.getConnection(connectionPool.getMasterUrl(),connectionPool.getUser(),connectionPool.getPassword());
-            boolean available = testMasterDB.isValid(1);
-            if (available==true) {
-                activeConnection = null;
-                getLog.info("MasterDB is available");
-            }
-        } catch (SQLException throwables) {
-            getLog.error("MasterDB is not available ",throwables);
-            throwables.printStackTrace();
-        }
     }
 }
