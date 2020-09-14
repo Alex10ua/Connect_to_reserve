3d46c53cf0393352d9fd49e371f377fd89254f30
alex <sasha.lazoriak@gmail.com>
2020-09-14 02:51:22 +0200

    [add] Added comments

--- src/main/java/app/SearchInLogs.java
+++ src/main/java/app/SearchInLogs.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class SearchInLogs {
-    List<String> list = Files.readAllLines(new File("Test_task_app.log").toPath(), Charset.defaultCharset() );
+    List<String> list = Files.readAllLines(new File("Test_task_app.log").toPath(), Charset.defaultCharset() );// read log file and write data into list
 
     public SearchInLogs() throws IOException {
     }
@@ -19,9 +19,9 @@ public class SearchInLogs {
         Scanner scanner = new Scanner(System.in);
         String searchingWord = scanner.nextLine();
         for (String element:list) {
-               boolean check =  element.contains(searchingWord);// get status
+               boolean check =  element.contains(searchingWord);// get status of containing word in list
                 if (check){
-                    listToShow.add(element);
+                    listToShow.add(element);//write the found item to the list
                 }
         }
         listToShow.forEach(element->{       //sout a new list with results
